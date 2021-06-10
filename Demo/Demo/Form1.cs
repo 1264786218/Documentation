8ddff2967b8fd93ba127ca1af2ee6e0325efb0cf
LanTian <33803367+1264786218@users.noreply.github.com>
2021-06-10 21:36:58 +0800

    Add files via upload

---
+++ Demo/Demo/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            WpfApplication2.MainWindow wpfwindow = new WpfApplication2.MainWindow();
+            Console.WriteLine("I am here!");
+            wpfwindow.ShowDialog();
+        }
+    }
+}
